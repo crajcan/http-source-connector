ec67be4fd00413f2a76151486a41a9758d85bd37
Carson Rajcan <carajcan@gmail.com>
2023-05-30 09:16:15 -0500

    Add unit test for wrapper stream

--- crates/http-source/src/http_response_stream.rs
+++ crates/http-source/src/http_response_stream.rs
@@ -38,29 +38,36 @@ pub fn chunk_http_stream<'a>(
     res.boxed_local()
 }
 
-// #[cfg(test)]
-// mod test {
-//     use std::boxed;
+#[cfg(test)]
+mod test {
+    use futures::{stream::LocalBoxStream, StreamExt};
+    use std::boxed;
 
+    #[async_std::test]
+    async fn test_chunk_http_stream_concatenates_chunks() {
+        let inner_stream = futures::stream::iter(vec![
+            Ok(bytes::Bytes::from("hello")),
+            Ok(bytes::Bytes::from(" world")),
+            Ok(bytes::Bytes::from("!")),
+            Ok(bytes::Bytes::from("Welcome")),
+            Ok(bytes::Bytes::from(" to ")),
+            Ok(bytes::Bytes::from("NY!")),
+        ]);
+        let boxed = inner_stream.boxed_local();
 
-//     #[async_std::test]
-//     async fn test_chunk_http_stream_concatenates_chunks() {
-//         let inner_stream = futures::stream::iter(vec![
-//             Ok(bytes::Bytes::from("hello")),
-//             Ok(bytes::Bytes::from(" world")),
-//             Ok(bytes::Bytes::from("!")),
-//             Ok(bytes::Bytes::from("Welcome")),
-//             Ok(bytes::Bytes::from(" to ")),
-//             Ok(bytes::Bytes::from("NY!")),
-//         ]);
-//         let boxed = inner_stream.boxed_local();
+        let mut chunked_stream =
+            super::chunk_http_stream(boxed, "!".to_string());
 
-//         let chunked_stream = super::chunk_http_stream(boxed, "!".to_string());
+        let first_chunk = chunked_stream.next().await;
+        assert_eq!(
+            first_chunk.unwrap().unwrap(),
+            bytes::Bytes::from("hello world!")
+        );
 
-//         let Some(first_chunk) = chunked_stream.next().await;
-//         assert_eq!(first_chunk.unwrap(), bytes::Bytes::from("hello world!"));
-
-//         let Some(second_chunk) = chunked_stream.next().await;
-//         assert_eq!(second_chunk.unwrap(), bytes::Bytes::from("Welcome to NY!"));
-//     }
-// }
+        let second_chunk = chunked_stream.next().await;
+        assert_eq!(
+            second_chunk.unwrap().unwrap(),
+            bytes::Bytes::from("Welcome to NY!")
+        );
+    }
+}
